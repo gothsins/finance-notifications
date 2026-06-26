59cd47a772e0d56074020f28e6bfaf93fe8923ef
gothsins <gui.starkteam@gmail.com>
2026-06-25 23:19:47 -0300

    chore: initial project setup

---
+++ src/main/java/com/guilherme/finance_notifications/FinanceNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.guilherme.finance_notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceNotificationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guilherme/finance_notifications/FinanceNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilherme.finance_notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceNotificationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
